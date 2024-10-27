e9391c19fbfabeeef03cc180629670779f96e4f6
maximmacarone <maximmacarone@gmail.com>
2024-10-27 12:56:18 +0300

    Initial Commit

---
+++ lapkiTests/lapkiTests.swift
@@ -0,0 +1,17 @@
+//
+//  lapkiTests.swift
+//  lapkiTests
+//
+//  Created by Maxim Makarenkov on 27.10.2024.
+//
+
+import Testing
+@testable import lapki
+
+struct lapkiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
