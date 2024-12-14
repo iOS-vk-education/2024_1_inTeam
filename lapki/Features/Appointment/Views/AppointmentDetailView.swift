b60e7abe96d46b7e4aacdfc99756406fb0e2de25
MaximMacarone <maximmacarone@gmail.com>
2024-12-15 00:39:14 +0300

    xcodegen update

---
+++ lapki/Features/Appointment/Views/AppointmentDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  AppointmentDetailView.swift
+//  lapki
+//
+//  Created by Maxim Makarenkov on 14.12.2024.
+//
+
+import SwiftUI
+
+struct AppointmentDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppointmentDetailView()
+}
---
+++ lapki/Features/Appointment/Views/UpdateAppointmentView.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateAppointmentView.swift
+//  lapki
+//
+//  Created by Maxim Makarenkov on 14.12.2024.
+//
+
+import SwiftUI
+
+struct UpdateAppointmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateAppointmentView()
+}
